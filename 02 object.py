2cb14930c7163cb99f5da45d0a46c9ead89c61a1
Syed Zaman Mostafiz <zamanwebdev>
2020-02-29 14:38:04 +0600

    1/174 01. Python Bangla Tutorial || Hello World || Code Hunter Learning with Practice Program Completed.

--- 02 object.py
+++ 02 object.py
@@ -1,2 +1,21 @@
 # Object Oriented Programming 
 # Object
+'''
+i = 10
+
+str = "ZamanWebDeveloper"
+
+list = [1,2,3,4,5,6]
+
+list.append(6)
+
+str.__len__()
+'''
+
+class Vehicle:
+	pass
+
+car = Vehicle()
+car.name = 'toyota'
+car.wheel = 4
+car.driver = 'solimuddin'
