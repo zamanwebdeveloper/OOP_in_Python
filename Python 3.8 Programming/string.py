942ecf52e5deefdc23764b07f20a49adf76657c0
Syed Zaman Mostafiz <zamanwebdev>
2020-03-01 21:34:40 +0600

    5/174 05. Python Bangla Tutorial || String || Change Case || Code Hunter Learning with Practice Program Completed.

---
+++ Python 3.8 Programming/string.py
@@ -0,0 +1,19 @@
+a = "Hello"
+b = "World"
+print(a + b)
+print(a,b,100)
+
+a = "this is a string"
+b = "THIS IS a stRING"
+
+# Capitalize
+cap = a.capitalize()
+print("Capitalize :",cap)
+
+# Uppercase
+up = a.upper()
+print("Uppercase :",up)
+
+# Lowercase
+low = b.lower()
+print("Lowercase :",low)
