4eaef3e853276b2d9027c092eb83bfcac04a5148
Syed Zaman Mostafiz <zamanwebdev>
2020-03-01 21:10:40 +0600

    3/174 03. Python Bangla Tutorial || Comments || Code Hunter Learning with Practice Program Completed.

---
+++ Python 3.8 Programming/math.py
@@ -0,0 +1,19 @@
+a = 10
+b = 5
+# sum, sub, multi, div
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print("summation \n",a,"+",b,"=",c)
+print("\nsubtract \n",a,"-",b,"=",d)
+print("\nmultiple \n",a,"*",b,"=",e)
+print("\ndivided \n",a,"/",b,"=",f)
+'''
+The name of my country name is Bangladesh
+My University Name is Dhaka International University
+My name is Syed Zaman Mostafiz
+'''
+a = 6
+b = 9
+print(a+b)#a+b = 6+9 = 15
