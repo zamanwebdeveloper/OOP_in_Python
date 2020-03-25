a51253c3c41eeddc8c3c4b03710c18fab7517e0f
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2020-03-25 20:36:22 +0600

    53/174 53. Python Bangla Tutorial || Regular Expression - Group Learning with Practice Program Completed.

--- Python 3.8 Programming/regularExpretion.py
+++ Python 3.8 Programming/regularExpretion.py
@@ -7,11 +7,16 @@ a = "My name is Zaman and i am a python programmer"
 # . means match any character * means zero or more
 # flags <optional>
 # re.I means case insensitive
-p = re.search('.*And.*',a,re.I)
-p = re.search('.*is.*',a,re.I)
-p = re.search('.*iss.*',a,re.I)
+# p = re.search('.*is.*',a,re.I)
+# p = re.search('.*iss.*',a,re.I)
+p = re.search('(.*)And (.*)',a,re.I)
+
 if p:
     print('Found')
+    print(p.group())
+    print(p.group(1),'After capitalize = ',p.group(1).capitalize())
+    print(p.group(2),': After = ',p.group(2).upper())
+    # print(p.group(2))
 else:
     print('Not Found')
 
