19a8475172e3509adb84a55d2f37cae77e310a44
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2020-03-30 15:18:42 +0600

    68/174 68. Python || Enumerate Built-in Method Learning with Practice Program Completed.

---
+++ Python 3.8 Programming/Enumerate.py
@@ -0,0 +1,5 @@
+# Enumerate
+a = [2,4,6,8,10]
+for i,j in enumerate(a):
+    print('index = ',i,'=> value = ',j,a[i]==j)
+print(a[3] == 8)
---
+++ Python 3.8 Programming/Set-UnionIntersection.py
@@ -0,0 +1,9 @@
+# Set - Union and Intersection
+set1 = {1,3,5,7,4}
+set2 = {2,4,6,7}
+# union = U intersection = /\
+print(set1.union(set2))
+print(set2.union(set1))
+print(set1.intersection(set2))
+print(set2.intersection(set1))
+
