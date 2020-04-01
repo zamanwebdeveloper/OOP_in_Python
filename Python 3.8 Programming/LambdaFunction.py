4d8dc67e33faf7a6d2bc305ba650b68ef6f2f284
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2020-04-01 18:07:22 +0600

    73/174 73. Python || Lambda Function Learning with Practice Program Completed.

---
+++ Python 3.8 Programming/LambdaFunction.py
@@ -0,0 +1,7 @@
+# Lambda Function
+# def haha(x):
+#     return x+1
+a = lambda x : x+1
+a = lambda x : x**2
+print(a(3))
+
---
+++ Python 3.8 Programming/PermutationComprehension.py
@@ -0,0 +1,8 @@
+# Permutation Using Comprehension
+# (a,b)(a,b) = (a,a)(a,b)(b,a)b,b)
+a = ('a','b')
+b = (2,3)
+s = {(x+y) for x in a for y in a }
+s = {(x,y) for x in a for y in b }
+print(s)
+
