231f903c108ed41c560526d72190637ede63ef88
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2020-05-08 19:19:42 +0600

    After Windows Setup Install the git

---
+++ 96.174 Inheritance - Indirect.py
@@ -0,0 +1,15 @@
+# Inheritance - Indirect
+class A:
+    def frist(self):
+        print('method - A')
+class B(A):
+    def second(self):
+        print('method - B')
+class C(B):
+    def third(self):
+        print('method - C')
+D = C()
+D.frist()
+D.second()
+D.third()
+
